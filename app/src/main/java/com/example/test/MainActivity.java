90cf436cea3e1cdc7baf8d9b2ce171d495aa45c7
Rickyy1 <90424819+Rickyy1@users.noreply.github.com>
2023-12-06 21:19:19 -0500

    Added additional columns: available and total spots plus handicap spots as well.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -86,11 +86,17 @@ public class MainActivity extends AppCompatActivity {
                 String lotId = jsonObject.getString("lot_id");
                 String lotName = jsonObject.getString("lot_name");
                 String availableSpots = jsonObject.getString("available_spots");
+                String totalSpots = jsonObject.getString("total_spots");
+                String availableHandicapSpots = jsonObject.getString("available_handicap_spots");
+                String totalHandicapSpots = jsonObject.getString("total_handicap_spots");
 
                 // Append the information to the displayText StringBuilder
                 displayText.append("Lot ID: ").append(lotId).append("\n")
                         .append("Lot Name: ").append(lotName).append("\n")
-                        .append("Available Spots: ").append(availableSpots).append("\n\n");
+                        .append("Available Spots: ").append(availableSpots).append("\n")
+                        .append("Total Spots: ").append(totalSpots).append("\n")
+                        .append("Available Handicap Spots: ").append(availableHandicapSpots).append("\n")
+                        .append("Total Handicap Spots: ").append(totalHandicapSpots).append("\n\n");
             }
 
             // Set or update the text with the concatenated information
